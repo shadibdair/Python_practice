3117b7b6fc65fcd35b8c92bb57a746d922ce12af
bdairshadi <bdairshadi@gmail.com>
2022-08-26 11:08:57 +0300

    First testing the code

---
+++ final-project-python/main.py
@@ -0,0 +1,10 @@
+import random
+
+
+alphabet_ = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+random.shuffle(alphabet_)
+
+
+
+print(alphabet_)
+
