98cfd9f415ab77e520f22e3da7ce84b7ecdf54a9
bdairshadi <bdairshadi@gmail.com>
2022-08-27 19:21:17 +0300

    Done with function enc_key

--- final-project-python/enc/enckey.py
+++ final-project-python/enc/enckey.py
@@ -4,12 +4,12 @@ import string
 
 def new_enc_key():
     encryption = list(string.ascii_lowercase)
-    print("enc: ", encryption)
-    random.shuffle(encryption)
-    #decryption = encryption
-    decryption = ''.join(encryption)
-    print("dec: ", decryption)
-    print("\n \n")
-    key = {encryption[i]:decryption[i] for i in range(len(encryption))}
-    print(key)
-    print("A new key called {} was created")
+    #print("enc: ", encryption)
+
+    letters_list = list(encryption)
+    shuffled = random.shuffle(letters_list)
+    #decryption = ''.join(encryption)
+    #print("dec: ", letters_list)
+    # This is the Key that I need to encrypt the words
+    key = {encryption[i]:letters_list[i] for i in range(len(encryption))}
+    #print(f"A new key called {name} was created")
--- final-project-python/enc/subs.py
+++ final-project-python/enc/subs.py
@@ -1,4 +1,5 @@
 # Imports
+from curses import keyname
 import random
 import string
 import enckey
@@ -16,6 +17,7 @@ def enc_init():
         'save':enc_save
         }
     return commands
+
 def main_cli():
     commands = enc_init()
     cli_end = False
@@ -23,26 +25,30 @@ def main_cli():
     while not cli_end:
         # Output subs> everytime
         cmd_str = input('subs>')
+        global cmd 
         cmd = cmd_str.split()
         # Check If cmd not empty and in place [0] have word 'done' 
         if cmd and cmd[0] == 'done':
             cli_end = True
         if cmd:
             commands[cmd[0]](cmd[1:])
+        
 # Function save the Key inside File
-def enc_save(self):
+def enc_save(*param):
     save_key = encsave.save_key()
     print(save_key)
 # Function that get the Key inside the File to use it for Enc/Dec
-def enc_load(self):
+def enc_load(*param):
     load_key = encload.load_key()
     print(load_key)
 # Function that create the key
-def enc_key(self):
+def enc_key(*param):
     the_new_key = enckey.new_enc_key()
-    print(the_new_key)
+    name = cmd[1]
+    #name = enckey.new_enc_key(key)
+    print(f"A new key called {name} was created")
 # Function that give us more info, status.
-def enc_info(self):
+def enc_info(*param):
     info_key = encinfo.info_key()
     print(info_key)
 
