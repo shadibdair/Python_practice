4837268fd6dbc075491598b01ff7fb6b341e08c6
bdairshadi <bdairshadi@gmail.com>
2022-08-27 18:08:20 +0300

    Fixing the functions

--- final-project-python/enc/subs.py
+++ final-project-python/enc/subs.py
@@ -16,8 +16,6 @@ def enc_init():
         'save':enc_save
         }
     return commands
-
-
 def main_cli():
     commands = enc_init()
     cli_end = False
@@ -31,22 +29,22 @@ def main_cli():
             cli_end = True
         if cmd:
             commands[cmd[0]](cmd[1:])
-# ------------
-
+# Function save the Key inside File
 def enc_save(self):
     save_key = encsave.save_key()
     print(save_key)
-
+# Function that get the Key inside the File to use it for Enc/Dec
 def enc_load(self):
     load_key = encload.load_key()
     print(load_key)
-
+# Function that create the key
 def enc_key(self):
     the_new_key = enckey.new_enc_key()
     print(the_new_key)
-
+# Function that give us more info, status.
 def enc_info(self):
     info_key = encinfo.info_key()
     print(info_key)
 
+# Calling the main function to start the program.
 main_cli()
