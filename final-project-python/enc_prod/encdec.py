a94de065fb3832f158894f95d5edb76b9e8b39bc
bdairshadi <bdairshadi@gmail.com>
2022-08-31 00:57:04 +0300

    Added decription

--- final-project-python/enc_prod/encdec.py
+++ final-project-python/enc_prod/encdec.py
@@ -1,16 +1,23 @@
 # Function that decrypt the text dec
 def encjsonconvert(dic, f_json, t_json):
-    str_enc = ''
-    with open(f_json, "r") as fm:
-        str = fm.read()
-        list_str = list(str)
-        for letter in list_str:
-            if letter in dic:
-                str_enc += dic[letter]
-            else:
-                str_enc += letter
-    with open(t_json, "w") as ft:
-        ft.write(str_enc)
-    return 0
-
+    string_ = ''
+    try:
+        with open(f_json, "r") as fm:
+            str = fm.read()
+            list_str = list(str)
+            for letter in list_str:
+                if letter in dic:
+                    string_ += dic[letter]
+                else:
+                    string_ += letter
+        with open(t_json, "w") as ft:
+            ft.write(string_)
+    except FileNotFoundError as e:
+        print(f"file not found: {e}")
+        return 1
+    except Exception as e:
+        print(f"Other error {e}")
+        return 1
+    else:
+        return 0
 
--- final-project-python/enc_prod/encjsonconvert.py
+++ final-project-python/enc_prod/encjsonconvert.py
@@ -2,16 +2,34 @@
 import json
 #----------
 
-def from_json(file, *more):
-       with open(file, "r") as f:
-        json_object = f.read()
-        dic = json.loads(json_object)
-        return dic["enc"], dic["dec"], dic["nameKey"]
-
 def to_json(dic_enc, dic_dec, file, nameKey):
     dic = {"enc": dic_enc, "dec": dic_dec, "nameKey": nameKey}
     json_object = json.dumps(dic)
-    with open(file, "w") as f:
-        f.write(json_object)
+    try:
+        with open(file, "w") as f:
+            f.write(json_object)
+    except FileNotFoundError as e:
+        print(f"file not found: {e}")
+        return 1
+    except Exception as e:
+        print(f"Other error {e}")
+        return 1
     return 0
 
+
+def from_json(file, *more):
+    try:
+        with open(file, "r") as f:
+            json_object = f.read()
+            dic = json.loads(json_object)
+            return dic["enc"], dic["dec"], dic["nameKey"]
+    except FileNotFoundError:
+        print("There's no key")
+        return (None, None, None)
+    except ValueError:
+        print("There's no key")
+        return (None, None, None)
+    except Exception as e:
+        if not more:
+            print(f"Other error {e}")
+        return (None, None, None)
